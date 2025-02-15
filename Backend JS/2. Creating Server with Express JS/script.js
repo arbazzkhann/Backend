eb26c12f48f2754690a516ff23f54262e4b68684
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-02-15 13:30:30 +0530

    creating routes with express js

--- Backend JS/2. Creating Server with Express JS/script.js
+++ Backend JS/2. Creating Server with Express JS/script.js
@@ -5,4 +5,14 @@ app.get('/', function(request, response) {
     response.send('Hello world');
 })
 
+app.get('/contact', function(request, response) {
+    response.send('Contact page.');
+})
+
+
+app.get('/about', function(request, response) {
+    response.send('About page.');
+})
+
+
 app.listen(3000);
