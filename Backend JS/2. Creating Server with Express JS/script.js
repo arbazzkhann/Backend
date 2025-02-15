42d182dafa05213c4b0f414dcac4b57da1908df1
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-02-15 13:40:34 +0530

    use function in express which is middleware precticing

--- Backend JS/2. Creating Server with Express JS/script.js
+++ Backend JS/2. Creating Server with Express JS/script.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 
+
+//Middleware
+//1. middleware is run before route which means middleware with the process before route done.
+//2. after middleware route should be done.
+//3. we can do multiple middleware in same code. 
+app.use(function(request, response, next) {
+    console.log("Hello console.");
+    next();
+});
+
+app.use(function(request, response, next) {
+    console.log("Hello console 2.");
+})
+
+
 app.get('/', function(request, response) {
     response.send('Hello world');
 })
