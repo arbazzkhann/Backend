e5d579c1606ec6c6a14948e05a26bb834e0270c1
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-02-19 19:39:17 +0530

    setting up index and css for / route

--- Backend JS/6. Error Handling in Express/script.js
+++ Backend JS/6. Error Handling in Express/script.js
@@ -1,3 +1,22 @@
+/*
+Video Link with Time Stamp: https://youtu.be/pKJ4GGyDgJo?t=4944
+
+    1. copy that (only for error example):
+
+        function errorHandler (err, req, res, next) {
+        if (res.headersSent) {
+            return next(err)
+        }
+        res.status(500)
+        res.render('error', { error: err })
+
+    2. 
+}
+
+
+
+*/
+
 const express = require('express');
 const app = express();
 
@@ -13,5 +32,13 @@ app.set('view engine', "ejs");
 app.get("/", function(request, response) {
     response.render("index", {name: "Arbaz"});
 });
+
+app.use(function errorHandler (err, req, res, next) {
+    if (res.headersSent) {
+      return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+  })
 
 app.listen(3000);
