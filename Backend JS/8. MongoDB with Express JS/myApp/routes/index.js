ba31ebd1a66ab756fe1df7ce4771da21effe5799
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-02-23 20:09:01 +0530

    .findOne

--- Backend JS/8. MongoDB with Express JS/myApp/routes/index.js
+++ Backend JS/8. MongoDB with Express JS/myApp/routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-//creating user with express
+// //creating user with express
 // router.get('/create', async function(req, res, next) {
 //   const createdUser = await userModel.create({
 //     username: "xr_arbaz",
@@ -19,11 +19,20 @@ router.get('/', function(req, res, next) {
 //   res.send(createdUser);
 // });
 
-//printing all userData with (.find)
+
+////(.find) Returns all documents
+// router.get('/allusers', async function(req, res, next) {
+//   const allUsers = await userModel.find();
+//   res.send(allUsers);
+// })
+
+
+////(.findOne) Returns only one document
 router.get('/allusers', async function(req, res, next) {
-  const allUsers = await userModel.find();
+  const allUsers = await userModel.findOne({name: "Arbaz Khan"});
   res.send(allUsers);
 })
+
 
 
 module.exports = router;
