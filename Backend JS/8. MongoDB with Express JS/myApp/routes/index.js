b06b688474438463a3e0c52017c32803d7fd14a4
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-02-23 19:54:32 +0530

    object creation in express with mongodb (mongoose)

--- Backend JS/8. MongoDB with Express JS/myApp/routes/index.js
+++ Backend JS/8. MongoDB with Express JS/myApp/routes/index.js
@@ -1,3 +1,5 @@
+//Video link: https://youtu.be/ZpszSj3ziQk?t=3639
+
 var express = require('express');
 var router = express.Router();
 const userModel = require("./users");
@@ -6,5 +8,16 @@ const userModel = require("./users");
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//creating user with express
+router.get('/create', async function(req, res, next) {
+  const createdUser = await userModel.create({
+    username: "xr_arbaz",
+    name: "Arbaz Khan",
+    age: 25
+  });
+  res.send(createdUser);
+});
+
 
 module.exports = router;
