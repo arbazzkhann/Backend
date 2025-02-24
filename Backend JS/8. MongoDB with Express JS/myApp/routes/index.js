d63d5629b881866c21be43d74a15f2330ddcec39
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-02-24 16:46:19 +0530

    mongodb - deleteOne, findOneAndDelete

--- Backend JS/8. MongoDB with Express JS/myApp/routes/index.js
+++ Backend JS/8. MongoDB with Express JS/myApp/routes/index.js
@@ -9,30 +9,45 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// //creating user with express
-// router.get('/create', async function(req, res, next) {
-//   const createdUser = await userModel.create({
-//     username: "xr_arbaz",
-//     name: "Arbaz Khan",
-//     age: 25
-//   });
-//   res.send(createdUser);
-// });
+////creating user with express
+router.get('/create', async function(req, res, next) {
+  const createdUser = await userModel.create({
+    username: "vijeta243",
+    name: "Vijeta Sharma",
+    age: 28
+  });
+  res.send(createdUser);
+});
 
 
 ////(.find) Returns all documents
-// router.get('/allusers', async function(req, res, next) {
-//   const allUsers = await userModel.find();
-//   res.send(allUsers);
-// })
-
-
-////(.findOne) Returns only one document
 router.get('/allusers', async function(req, res, next) {
-  const allUsers = await userModel.findOne({name: "Arbaz Khan"});
+  const allUsers = await userModel.find();
   res.send(allUsers);
 })
 
+
+////(.findOne) Returns only one document
+router.get('/oneuser', async function(req, res, next) {
+  const oneuser = await userModel.findOne({name: "Arbaz Khan"});
+  res.send(oneuser);
+})
+
+
+////(.deleteOne) remove one document
+router.get('/deleteOne', async function(req, res, next) {
+  const removeUser = await userModel.deleteOne();
+  res.send(removeUser);
+})
+
+
+////(.findOneAndDelete) remove one document
+router.get('/findOneAndDelete', async function(req, res, next) {
+  const findOneAndDelete = await userModel.findOneAndDelete({
+    age: 22
+  });
+  res.send(findOneAndDelete);
+})
 
 
 module.exports = router;
