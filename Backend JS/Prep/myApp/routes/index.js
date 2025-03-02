b1ec6cac8ddee77515af5d8f727dbb55e68502ca
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-02 20:10:54 +0530

    final prectice of mongodb connect with express

--- Backend JS/Prep/myApp/routes/index.js
+++ Backend JS/Prep/myApp/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+let dbClass10 = require("./users")
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get("/createclass10", async function(req, res, next) {
+  const student = await dbClass10.create({
+    name: "Madhav",
+    age: 17,
+    rollNo: 23,
+    medium: "English"
+  });
+  res.send(student);
 });
 
 module.exports = router;
