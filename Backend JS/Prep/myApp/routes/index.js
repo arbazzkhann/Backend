fb370e36dc00f31a277af5d02dc2a17e5ebfd069
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-03 16:18:02 +0530

    creating express project for prectice

--- Backend JS/Prep/myApp/routes/index.js
+++ Backend JS/Prep/myApp/routes/index.js
@@ -1,49 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Home page");
+  res.render('index', { title: 'Express' });
 });
-
-router.get('/ban', function(req, res, next) {
-  req.session.ban = true;
-  res.send("You are banned now");
-});
-
-
-router.get('/isban', function(req, res, next) {
-  if(req.session.ban == true) {
-    res.send("you are banned");
-  }
-  else {
-    res.send("Not banned");
-  }
-
-});
-
-router.get('/removeban', function(req, res, next) {
-  if(req.session.ban == true) {
-    req.session.ban == false;
-    res.send("You are unbanned");
-  }
-  else {
-    res.send("Not ban");
-  }
-});
-
-
-
-//cookies
-router.get("/createCookie", function(req, res, next) {
-  res.cookie("arbazAge", 22);
-  res.send(req.cookies);
-});
-
-router.get("/showCookies", function(req, res, next) {
-  console.log(req.cookies)
-  res.send("See console.");
-});
-
-
 
 module.exports = router;
