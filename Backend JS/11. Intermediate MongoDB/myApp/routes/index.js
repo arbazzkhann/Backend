ba751bd538566d1b84b8225a2905b5edfbd40c75
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-05 12:55:36 +0530

    RegExp function, creating date in schema for mongoDB, start and end string

--- Backend JS/11. Intermediate MongoDB/myApp/routes/index.js
+++ Backend JS/11. Intermediate MongoDB/myApp/routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+//mongoose
+const userModel = require('./users');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//create
+router.get('/create', async function(req, res) {
+  const userData = await userModel.create({
+    username: "Arbaz Khan",
+    nickname: "Arbaz",
+    description: "I am arbaz khan learning backend for web dev.",
+    categories: {
+      type: ["web dev", "development", "creative", "web sites", "backend", "database"]
+    }
+  });
+  res.send(userData);
+});
+
+//find
+router.get('/find', async function(req, res) {
+  const regexnew = RegExp("^Arbaz$", "i");
+  const userData = await userModel.find({nickname: regexnew})
+  res.send(userData);
+});
+
+
+//delete
+router.get('/delete', async function(req, res) {
+  // const userData = await userModel.deleteOne();
+  const userData = await userModel.deleteMany();
+  res.send(userData);
+});
+
+
+
+
 
 module.exports = router;
