c95b3b09a709c24d78c72813db8accebd2d61de1
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-05-15 12:17:22 +0530

    renaming

--- Backend By Sheryians Coding School/1. File System/script.js
+++ Backend By Sheryians Coding School/1. File System/script.js
@@ -1,9 +1,10 @@
-//writeFile
-//appendFile
-//copyFile
-//rename
-//unlink
-//mkdir
+//Create File (writeFile)
+//Append File (appendFile)
+//Copy File (copyFile)
+//Rename File (rename)
+//Remove File (unlink) 
+//Make Directory (mkdir)
+//Remove Directory (rmdir)
 
 
 const fs = require('fs');
