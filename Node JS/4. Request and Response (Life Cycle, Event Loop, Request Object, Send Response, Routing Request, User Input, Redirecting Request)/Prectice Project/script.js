13d18149db78bc0aab77deac92d93d5da76fe250
Arbaz Khan <arbazkha78az@gmail.com>
2025-07-16 19:49:23 +0530

    node.js prectice question

---
+++ Node JS/4. Request and Response (Life Cycle, Event Loop, Request Object, Send Response, Routing Request, User Input, Redirecting Request)/Prectice Project/script.js
@@ -0,0 +1,11 @@
+/*
+Question: Create a page that shows a navigation bar of Myntra with the following links:
+            A. Home
+            B. Men
+            C. Women
+            D. Kids
+            E. Cart
+
+            Clicking on each link page should navigate to that page and a welcome to selection text is shown there.
+*/
+
