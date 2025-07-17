95a59cf4e34d89769f540e9fb000f73e6215f9e7
Arbaz Khan <arbazkha78az@gmail.com>
2025-07-17 12:48:24 +0530

    simple prectice project of node js - creating navbar and returning html as per route

--- Node JS/4. Request and Response (Life Cycle, Event Loop, Request Object, Send Response, Routing Request, User Input, Redirecting Request)/Prectice Project/script.js
+++ Node JS/4. Request and Response (Life Cycle, Event Loop, Request Object, Send Response, Routing Request, User Input, Redirecting Request)/Prectice Project/script.js
@@ -9,3 +9,57 @@ Question: Create a page that shows a navigation bar of Myntra with the following
             Clicking on each link page should navigate to that page and a welcome to selection text is shown there.
 */
 
+const http = require('http');
+
+const PORT = 3000;
+
+http.createServer((req, res) => {
+    
+    if(req.url === '/') {
+        res.write
+        (`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Myntra</title>
+            </head>
+            <body>
+                <nav>
+                    <ul>
+                        <a href="/"><li>Home</li></a>
+                        <a href="/men"><li>Men</li></a>
+                        <a href="/women"><li>Women</li></a>
+                        <a href="/kids"><li>Kids</li></a>
+                        <a href="/cart"><li>Cart</li></a>
+                    </ul>
+                </nav>
+            </body>
+            </html>
+        `);
+        return res.end();
+    }
+    else if(req.url === '/men') {
+        res.write("<h1>This is men page.</h1>");
+        return res.end();
+    }
+    else if(req.url === '/women') {
+        res.write("<h1>This is women page.</h1>");
+        return res.end();
+    }
+    else if(req.url === '/kids') {
+        res.write("<h1>This is kids page.</h1>");
+        return res.end();
+    }
+    else if(req.url === '/cart') {
+        res.write("<h1>This is cart page.</h1>");
+        return res.end();
+    }
+    res.write("<h1>404 page not found.</h1>");
+    res.end();
+
+}).listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+});
+
