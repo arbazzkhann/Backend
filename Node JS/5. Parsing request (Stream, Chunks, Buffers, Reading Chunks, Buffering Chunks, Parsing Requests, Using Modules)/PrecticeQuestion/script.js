d80aee2a78c7805605444679aca1da5527a8fc8a
Arbaz Khan <arbazkha78az@gmail.com>
2025-07-19 18:45:22 +0530

    Node.js prectice question

---
+++ Node JS/5. Parsing request (Stream, Chunks, Buffers, Reading Chunks, Buffering Chunks, Parsing Requests, Using Modules)/PrecticeQuestion/script.js
@@ -0,0 +1,7 @@
+/*
+Question: Create a Calculator:-
+    1. Create a new Node.js project named "Calculator".
+    2. On the home page (route "/"), show a welcome message and a link to the calculator page.
+    3. On the "/calculator" page, display a form with two input fields and "Sum" button.
+    4. When the user clicks the "Sum" button, they should be taken to the "/calculate-result" page, which shows the sum of the two numbers.
+*/
