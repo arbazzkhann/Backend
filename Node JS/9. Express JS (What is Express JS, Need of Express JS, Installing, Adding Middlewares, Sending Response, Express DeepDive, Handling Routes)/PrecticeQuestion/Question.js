67752643b11215c3c2dc0e81fedce3cab0ed233e
Arbaz Khan <arbazkha78az@gmail.com>
2025-07-29 21:06:00 +0530

    prectice question for express.js

---
+++ Node JS/9. Express JS (What is Express JS, Need of Express JS, Installing, Adding Middlewares, Sending Response, Express DeepDive, Handling Routes)/PrecticeQuestion/Question.js
@@ -0,0 +1,11 @@
+/*
+
+Ques. Create a new project
+    1. Install nodemon and express.
+    2. Add two dummy middleware that logs request's path and request method respectively.
+    3. Add a third middleware that return a response.
+    4. Now add handling using two more middleware that handle path '/', a request to '/contact-us' page.
+    5. Contact us should return a form with name and email as imput fields that submits to /contact-us page also.
+    6. Also handle POST incoming request to '/contact-us' path using a separate middleware.
+
+*/
