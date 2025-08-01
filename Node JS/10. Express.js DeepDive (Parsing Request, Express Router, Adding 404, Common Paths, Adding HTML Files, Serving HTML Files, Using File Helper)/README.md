408e86eb956c0ac60dabcb86d82eab9fc110daf7
Arbaz Khan <arbazkha78az@gmail.com>
2025-08-01 16:56:18 +0530

    bodyParser usage

--- Node JS/10. Express.js DeepDive (Parsing Request, Express Router, Adding 404, Common Paths, Adding HTML Files, Serving HTML Files, Using File Helper)/README.md
+++ Node JS/10. Express.js DeepDive (Parsing Request, Express Router, Adding 404, Common Paths, Adding HTML Files, Serving HTML Files, Using File Helper)/README.md
@@ -8,3 +8,74 @@
 npm install body-parser
 ```
 
+#### Syntax body-parser:
+```js
+//module import
+const bodyParser = require('body-parser');
+
+//urlencoded middleware
+app.use(bodyParser.urlencoded());
+
+//use
+req.body._name_
+```
+
+#### Example:
+```js
+//external modules
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//url encoded
+app.use(bodyParser.urlencoded());
+
+// handling '/contact-us' on GET
+app.get('/', (req, res, next) => {
+    res.send(`
+        <html>
+            <head><title>Prectice Question</title></head>
+            <body>
+                <h2>Enter your details</h2>
+                <form action="/submitted" method="post">
+                    <label>Name: </label>
+                    <input name="name" type="text" placeholder="Enter full name"> 
+
+                    <br/><br/>
+
+                    <label>Email:</label>
+                    <input name="email" type="email" placeholder="Enter email address">
+                    
+                    <br/><br/>
+
+                    <button type="submit">Submit</button>
+                </form>
+            </body>
+        </html>
+    `);
+    next();
+});
+
+
+app.post('/submitted', (req, res, next) => {
+    //using body-parser
+    console.log(`Handling contact-us on POST request, url: ${req.url} and method: ${req.method}, ${req.body.name}`);
+    res.send(`
+        <html>
+            <head><title>Prectice Question</title></head>
+            <body>
+                <h2>Hello, ${req.body.name}</h2>
+                <h3>You are loggedin with ${req.body.email}</h3>
+            </body>
+        </html>
+    `);
+});
+
+
+//server listen
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Your server is running on http://localhost:${PORT}`);
+});
+```
--- Node JS/10. Express.js DeepDive (Parsing Request, Express Router, Adding 404, Common Paths, Adding HTML Files, Serving HTML Files, Using File Helper)/app.js
+++ Node JS/10. Express.js DeepDive (Parsing Request, Express Router, Adding 404, Common Paths, Adding HTML Files, Serving HTML Files, Using File Helper)/app.js
@@ -1,26 +1,112 @@
+// //external modules
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+// app.use(bodyParser.urlencoded());
+
+// // handling '/contact-us' on GET
+// app.get('/contact-us', (req, res, next) => {
+//     res.send(`
+//         <html>
+//             <head><title>Prectice Question</title></head>
+//             <body>
+//                 <h2>Enter your details</h2>
+//                 <form action="/contact-us" method="post">
+//                     <label>Name: </label>
+//                     <input name="name" type="text" placeholder="Enter full name"> 
+
+//                     <br/><br/>
+
+//                     <label>Email:</label>
+//                     <input name="email" type="email" placeholder="Enter email address">
+                    
+//                     <br/><br/>
+
+//                     <button type="submit">Submit</button>
+//                 </form>
+//             </body>
+//         </html>
+//     `);
+//     next();
+// });
+
+
+// app.post('/contact-us', (req, res, next) => {
+//     //using body-parser
+//     console.log(`Handling contact-us on POST request, url: ${req.url} and method: ${req.method}, ${req.body.name}`);
+//     res.send(`
+//         <html>
+//             <head><title>Prectice Question</title></head>
+//             <body>
+//                 <h2>Hello, ${req.body.name}</h2>
+//                 <h3>You are loggedin with ${req.body.email}</h3>
+//             </body>
+//         </html>
+//     `);
+// });
+
+
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log(`Your server is running on http://localhost:${PORT}`);
+// });
+
+
+
+
+//external modules
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-//first middleware
-app.use('/', (req, res, next) => {
-    console.log(`first middleware, method: ${req.method} and url: ${req.url}`)
+app.use(bodyParser.urlencoded());
+
+// handling '/contact-us' on GET
+app.get('/', (req, res, next) => {
+    res.send(`
+        <html>
+            <head><title>Prectice Question</title></head>
+            <body>
+                <h2>Enter your details</h2>
+                <form action="/submitted" method="post">
+                    <label>Name: </label>
+                    <input name="name" type="text" placeholder="Enter full name"> 
+
+                    <br/><br/>
+
+                    <label>Email:</label>
+                    <input name="email" type="email" placeholder="Enter email address">
+                    
+                    <br/><br/>
+
+                    <button type="submit">Submit</button>
+                </form>
+            </body>
+        </html>
+    `);
     next();
 });
 
-//second middleware
-app.use('/submit-details', (req, res, next) => {
-    console.log(`second middleware, submit-details page`);
+
+app.post('/submitted', (req, res, next) => {
+    //using body-parser
+    console.log(`Handling contact-us on POST request, url: ${req.url} and method: ${req.method}, ${req.body.name}`);
     res.send(`
         <html>
-            <head><title>Express Introduction</title></head>
+            <head><title>Prectice Question</title></head>
             <body>
-                <h1>This is submit-details page.</h1>
+                <h2>Hello, ${req.body.name}</h2>
+                <h3>You are loggedin with ${req.body.email}</h3>
             </body>
-        </html>    
+        </html>
     `);
-})
+});
 
 
-app.listen(3000, () => {
-    console.log(`Server is running on http://localhost:3000`);
-})
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Your server is running on http://localhost:${PORT}`);
+});
