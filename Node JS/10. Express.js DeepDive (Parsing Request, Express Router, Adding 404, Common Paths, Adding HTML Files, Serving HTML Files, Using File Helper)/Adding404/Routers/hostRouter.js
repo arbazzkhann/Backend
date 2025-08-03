64f225ef850f71f964489d426cbdff97602f3a60
Arbaz Khan <arbazkha78az@gmail.com>
2025-08-03 11:51:29 +0530

    giving html from .html file

--- Node JS/10. Express.js DeepDive (Parsing Request, Express Router, Adding 404, Common Paths, Adding HTML Files, Serving HTML Files, Using File Helper)/Adding404/Routers/hostRouter.js
+++ Node JS/10. Express.js DeepDive (Parsing Request, Express Router, Adding 404, Common Paths, Adding HTML Files, Serving HTML Files, Using File Helper)/Adding404/Routers/hostRouter.js
@@ -1,38 +1,20 @@
+//core modules
+const path = require('path');
+
+//external modules
 const express = require('express');
+
 const hostRouter = express.Router();
 
 
 //form GET
 hostRouter.get('/add-home', (req, res, next) => {
-    res.send(`
-        <html>
-            <head><title>Add Home</title></head>
-            <body>
-                <h2>ADD HOME PAGE</h2>
-                <form action="/host/add-home" method="POST">
-                    <label>House Name: </label>
-                    <input name="houseName" type="text" placeholder="Enter house name"> 
-
-                    <br/><br/>
-
-                    <button type="submit">Submit</button>
-                </form>
-            </body>
-        </html>
-    `);
+    res.sendFile(path.join(__dirname, "../views/add-home-get.html"));
 });
 
 //form POST
 hostRouter.post('/add-home', (req, res, next) => {
-    res.send(`
-        <html>
-            <head><title>Add Home</title></head>
-            <body>
-                <h2>Home register successfully</h2>
-                <h3>House name is ${req.body.houseName}</h3>
-            </body>
-        </html>
-    `);
+    res.sendFile(path.join(__dirname, "../views/add-home-post.html"));
 });
 
 module.exports = hostRouter;
--- Node JS/10. Express.js DeepDive (Parsing Request, Express Router, Adding 404, Common Paths, Adding HTML Files, Serving HTML Files, Using File Helper)/Adding404/app.js
+++ Node JS/10. Express.js DeepDive (Parsing Request, Express Router, Adding 404, Common Paths, Adding HTML Files, Serving HTML Files, Using File Helper)/Adding404/app.js
@@ -1,5 +1,6 @@
 //external modules
 const express = require('express');
+const path = require('path');
 
 //Router
 const userRouter = require('./Routers/userRouter');
@@ -23,7 +24,7 @@ app.use('/host', hostRouter);
 
 //404
 app.use((req, res, next) => {
-    res.status(404).send(`<h1>404 not found</h1>`);
+    res.status(404).sendFile(path.join(__dirname, "./views/404.html"));
 });
 
 app.listen(3000, () => {
