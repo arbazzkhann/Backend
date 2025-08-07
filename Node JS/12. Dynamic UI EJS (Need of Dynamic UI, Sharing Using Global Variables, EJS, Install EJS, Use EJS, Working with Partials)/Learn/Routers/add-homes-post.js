7f0a21b0f7236a866ccd6fa2b93c152f4aaf702b
Arbaz Khan <arbazkha78az@gmail.com>
2025-08-07 21:13:36 +0530

    making more input fields in add-homes page - (price, house photo link) and displaying on registered-homes page in form of card with tailwind css

--- Node JS/12. Dynamic UI EJS (Need of Dynamic UI, Sharing Using Global Variables, EJS, Install EJS, Use EJS, Working with Partials)/Learn/Routers/add-homes-post.js
+++ Node JS/12. Dynamic UI EJS (Need of Dynamic UI, Sharing Using Global Variables, EJS, Install EJS, Use EJS, Working with Partials)/Learn/Routers/add-homes-post.js
@@ -5,7 +5,13 @@ const addHomesPOSTRouter = express.Router();
 const registerdHouses = [];
 
 addHomesPOSTRouter.post('/add-homes', (req, res, next) => {
-    registerdHouses.push({houseName: req.body.houseName, houseState: req.body.houseState});
+    registerdHouses.push({ 
+        houseName: req.body.houseName, 
+        houseState: req.body.houseState, 
+        housePrice: req.body.housePrice, 
+        housePhoto: req.body.housePhoto,
+        housePhotoLink: req.body.housePhotoLink
+    });
     res.render('add-homes-post.ejs', {req});
 });
 
