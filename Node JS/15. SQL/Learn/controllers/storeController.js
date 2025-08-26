1264039fc49c7ac77a527c0c32d84c7c9e136f95
Arbaz Khan <arbazkha78az@gmail.com>
2025-08-26 16:21:59 +0530

    fixing first fetchAll function

--- Node JS/15. SQL/Learn/controllers/storeController.js
+++ Node JS/15. SQL/Learn/controllers/storeController.js
@@ -2,22 +2,30 @@
 const Favourite = require('../models/favourite');
 const Houses = require('../models/houses');
 
-const homeGET = (req, res, next) => {
+exports.homeGET = (req, res, next) => {
     console.log(`Handling / for get, request method is ${req.method}`);
     res.render("home.ejs", {req, activePath: '/'});
 }
 
-const registeredHouses = (req, res, next) => {
-    Houses.fetchAll(registeredHouses => {
-        res.render('store/registeredHouses.ejs', {registeredHouses, req, activePath: '/registered-houses'});
+exports.registeredHouses = (req, res, next) => {
+    Houses.fetchAll().then(([registeredHouses, fields]) => {
+        console.log(registeredHouses);
+        res.render('store/registeredHouses.ejs', {
+            registeredHouses, 
+            req, 
+            activePath: '/registered-houses'
+        });
+    })
+    .catch(err => {
+        console.log("error while db houses records: ", err);
     });
 }
 
-const BookingsGET = (req, res, next) => {
+exports.BookingsGET = (req, res, next) => {
     res.render('store/bookings.ejs', {registeredHouses, req, activePath: '/bookings'});
 }
 
-const FavouriteListGET = (req, res, next) => {
+exports.FavouriteListGET = (req, res, next) => {
     Favourite.getFavourites(favourites => {
         Houses.fetchAll((registeredHouses) => {
             const favouriteHouses = registeredHouses.filter(house =>
@@ -28,17 +36,17 @@ const FavouriteListGET = (req, res, next) => {
     });
 }
 
-const contactUsGET = (req, res, next) => {
+exports.contactUsGET = (req, res, next) => {
     res.render('store/contact-us-get.ejs', {req, activePath: '/contact-us'});
 }
 
-const contactUsPOST = (req, res, next) => {
+exports.contactUsPOST = (req, res, next) => {
     console.log(req.body);
     console.log(`Handling contact-us on POST request, url: ${req.url} and method: ${req.method}`);
     res.render('store/contact-us-post.ejs', {req, activePath: '/contact-us'});
 }
 
-const houseDetails = (req, res, next) => {
+exports.houseDetails = (req, res, next) => {
     const houseId = req.params.houseId;
 
     Houses.findById(houseId, (house) => {
@@ -52,7 +60,7 @@ const houseDetails = (req, res, next) => {
     });
 }
 
-const addToFavouritePOST = (req, res, next) => {
+exports.addToFavouritePOST = (req, res, next) => {
     console.log("Came to add to Favourite", req.body);
     Favourite.addToFavourite(req.body.houseId, err => {
         if(err) {
@@ -62,7 +70,7 @@ const addToFavouritePOST = (req, res, next) => {
     });
 }
 
-const removeFromFavouritePOST = (req, res, next) => {
+exports.removeFromFavouritePOST = (req, res, next) => {
     console.log("Came to remove from Favourite", req.body);
     Favourite.removeFromFavourite(req.body.houseId, (err) => {
         if (err && err.startsWith("Error")) {
@@ -74,15 +82,3 @@ const removeFromFavouritePOST = (req, res, next) => {
         res.redirect("/favourite-list");
     });
 };
-
-
-
-exports.homeGET = homeGET;
-exports.registeredHouses = registeredHouses;
-exports.BookingsGET = BookingsGET;
-exports.FavouriteListGET = FavouriteListGET;
-exports.contactUsGET = contactUsGET;
-exports.contactUsPOST = contactUsPOST;
-exports.houseDetails = houseDetails;
-exports.addToFavouritePOST = addToFavouritePOST;
-exports.removeFromFavouritePOST = removeFromFavouritePOST;
--- Node JS/15. SQL/Learn/models/houses.js
+++ Node JS/15. SQL/Learn/models/houses.js
@@ -1,12 +1,6 @@
-//core modules
-const fs = require('fs');
-const path = require('path');
+//database
+const db = require('../utils/databaseUtils');
 
-//local modules
-const absolutePath = require('../utils/pathUtils');
-
-//data file path
-const housesDataPath = path.join(absolutePath, 'data', 'houses.json');
 
 //Model
 module.exports = class House {
@@ -19,49 +13,19 @@ module.exports = class House {
 
     // //Save house data into the json file
     save(id) {
-        House.fetchAll(registeredHouses => {
-            if(id) { // edit house case
-                this.houseId = Number(id);
-                registeredHouses = registeredHouses.map(house => 
-                    String(house.houseId) === String(this.houseId) ? this : house
-                );
-            }
-            else { //add house case
-                this.houseId = Math.floor(10000000 + Math.random() * 90000000);
-                registeredHouses.push(this);
-            }
-            fs.writeFile(housesDataPath, JSON.stringify(registeredHouses), err => {
-                if(err) console.log('error occur: ', err);
-                else console.log("Save done!");
-            });
-        }) 
+        
     }
     
     //Retrieve all houses
-    static fetchAll(callback) {
-        fs.readFile(housesDataPath, (error, data) => {
-            // callback(!error ? JSON.parse(data) : []);
-            if(!error) {
-                callback(JSON.parse(data));
-            }
-            else {
-                console.log(error)
-                callback([]);
-            }
-        });
+    static fetchAll() {
+        return db.execute('SELECT * FROM houses')
     }
 
     static findById(houseId, callback) {
-        this.fetchAll((houses) => {
-            const houseFound = houses.find(house => house.houseId == houseId);
-            callback(houseFound);
-        });
+      
     }
 
     static deleteById(houseId, callback) {
-        this.fetchAll(houses => {
-            houses = houses.filter(house => house.houseId !== +houseId);
-            fs.writeFile(housesDataPath, JSON.stringify(houses), callback);
-        });
+      
     }
 }
