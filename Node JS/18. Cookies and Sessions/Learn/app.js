5f2da5bab116f7489465fae8be183f48b4ac00b2
Arbaz Khan <arbazkha78az@gmail.com>
2025-09-22 18:23:49 +0530

    replicating project from previous

---
+++ Node JS/18. Cookies and Sessions/Learn/app.js
@@ -0,0 +1,65 @@
+//external modules
+const express = require('express');
+
+//local modules
+const storeRouter = require('./Routers/user-routers/storeRouter.js');
+
+const hostRouter = require('./Routers/host-routers/hostRouter.js');
+
+//errors
+const { pageNotFount } = require('./controllers/errors.js');
+
+//Mongoose  
+const { default: mongoose } = require('mongoose');
+
+//MySQL
+// db.execute('SELECT * FROM houses')
+// .then(([rows, fields]) => {
+//     console.log('getting from database: ', rows);
+// })
+// .catch(err => {
+//     console.log("error while db houses records: ", err);
+// });
+
+//express app
+const app = express();
+
+//view engine
+app.set('view engine', 'ejs');
+
+//static files intigration
+// app.use(express.static(abosolutePath));
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//for req.body
+app.use(express.urlencoded());
+
+// handlig store routers
+app.use(storeRouter);
+
+// handling host routers
+app.use(hostRouter);
+
+//404 status
+app.use(pageNotFount);
+
+//server connect
+const PORT = 3000;
+
+
+//database path string
+const DB_PATH = "mongodb+srv://arbazfanda3:root@arbazkhan.nmsxldo.mongodb.net/mywebsite?retryWrites=true&w=majority&appName=ArbazKhan";
+
+//db connect with mongoose
+mongoose.connect(DB_PATH)
+.then(() => {
+    console.log(`Connected to MongoDB`);
+    app.listen(PORT, () => {
+        console.log(`Your server is running on http://localhost:${PORT}`);
+    });
+})
+.catch(err => {
+    console.log("Error while connecting to MongoDB: ", err);
+});
---
+++ Node JS/18. Cookies and Sessions/Learn/controllers/storeController.js
@@ -0,0 +1,96 @@
+//local modules
+const Favourite = require('../models/favourite');
+const House = require('../models/house');
+
+exports.homeGET = (req, res, next) => {
+    console.log(`Handling / for get, request method is ${req.method}`);
+    res.render("home.ejs", {req, activePath: '/'});
+}
+
+exports.registeredHouses = (req, res, next) => {
+    House.find().then(registeredHouses => {
+        console.log("registered Houses: ", registeredHouses);
+        res.render('store/registeredHouses.ejs', {
+            registeredHouses, 
+            req, 
+            activePath: '/registered-houses'
+        });
+    })
+    .catch(err => {
+        console.log("error while db houses records: ", err);
+    });
+}
+
+exports.BookingsGET = (req, res, next) => {
+    res.render('store/bookings.ejs', {registeredHouses, req, activePath: '/bookings'});
+}
+
+exports.FavouriteListGET = (req, res) => {
+    Favourite.find().then(favourites => {
+        favourites = favourites.map(fav => fav.houseId.toString());
+
+        House.find().then(registeredHouses => {
+            const favouriteHouses = registeredHouses.filter(house =>
+                favourites.includes(String(house._id))
+            );
+            res.render("store/favourite-list.ejs", {favouriteHouses, req, activePath: '/favourite-list'});
+        });
+    });
+}
+
+exports.contactUsGET = (req, res, next) => {
+    res.render('store/contact-us-get.ejs', {req, activePath: '/contact-us'});
+}
+
+exports.contactUsPOST = (req, res, next) => {
+    console.log(req.body);
+    console.log(`Handling contact-us on POST request, url: ${req.url} and method: ${req.method}`);
+    res.render('store/contact-us-post.ejs', {req, activePath: '/contact-us'});
+}
+
+exports.houseDetails = (req, res, next) => {
+    const houseId = req.params.houseId;
+
+    House.findById(houseId).then(house => {
+        if(!house) {
+            res.redirect("/registered-houses");
+        }
+        else {
+            console.log("House details found", house);
+            res.render('store/house-details.ejs', {req, houseId, activePath: '/house-details', house: house});
+        }
+    });
+}
+
+exports.addToFavouritePOST = (req, res, next) => {
+    const houseId = req.body.houseId;
+    console.log("House ID: ", houseId);
+
+    Favourite.findOne({houseId}).then(fav => {
+        if(fav) {
+            console.log("Already marked as favourite");
+        }
+        else {
+            fav = new Favourite({houseId});
+            fav.save().then(result => {
+                console.log("Added to favourite list: ", result);
+            });
+        }
+        res.redirect('/favourite-list')
+    }).catch(err => {
+        console.log("Error while add to favourite: ", err);
+    });
+}
+
+exports.removeFromFavouritePOST = (req, res, next) => {
+    console.log("Came to remove from Favourite", req.body);
+    const houseId = req.body.houseId;
+    Favourite.findOneAndDelete(houseId)
+    .then(result => {
+        console.log("Removed from favourites: ", result);
+    }).catch(err => {
+        console.log("Error occur while add to removing from favourite", err);
+    }).finally(() => {
+        res.redirect('/favourite-list');
+    })
+};
