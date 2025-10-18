8f770db7b1faf93dc8b490134c7634a2d9691f0f
Arbaz Khan <arbazkha78az@gmail.com>
2025-10-18 21:56:51 +0530

    installing and using "cors", making backend app.js structure for todo frontend

--- Node JS/22. Projects/1. Todo-App/todo-backend/app.js
+++ Node JS/22. Projects/1. Todo-App/todo-backend/app.js
@@ -3,33 +3,45 @@ const path = require("path");
 
 //external modules
 const express = require('express');
+const cors = require("cors");
 
 //local modules
 const { pageNotFount } = require('./controllers/errors.js');
-
-//express app
-const app = express();
+const todoRouter = require('./Routers/todoItemsRouter.js')
 
 //DB
 const DB_PATH = "mongodb+srv://arbazfanda3:root@arbazkhan.nmsxldo.mongodb.net/mywebsite?retryWrites=true&w=majority&appName=ArbazKhan";
 const { default: mongoose } = require('mongoose');
- 
+
+//express app
+const app = express();
+
+
 //static files intigration
 // app.use(express.static(abosolutePath));
 app.use(express.static(path.join(__dirname, "public")));
 
 
-//middleware for "URL" and "request method"
-app.use((req, res, next) => {
-    console.log(`Current URL: ${req.url} and method: ${req.method}`);
-    next();
-});
+
+app.use(cors());
+app.use(express.json());
+
+
+//todo
+app.use(todoRouter);
 
 //404 status
 app.use(pageNotFount);
 
 
 
+
+
+
+
+
+
+//Server and DB Start here
 //server connect
 const PORT = 3000;
 //db connect with mongoose
